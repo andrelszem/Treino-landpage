68e371fd3644fd1b3de7d635697f33a59d374ba5
andrelszem <andrelszem@gmail.com>
2025-11-13 23:10:06 -0300

    Update content paths in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./app/**/*.{js,jsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
